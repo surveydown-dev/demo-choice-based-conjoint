f710400bac01f0c75f14946d7db0129270d18c0b
John Helveston <john.helveston@gmail.com>
2024-10-14 22:55:59 -0400

    init data cleaning and modeling

--- make_choice_questions.R
+++ make_choice_questions.R
@@ -12,7 +12,7 @@ library(tidyverse)
 # Define profiles with attributes and levels
 profiles <- cbc_profiles(
     type      = c('Fuji', 'Gala', 'Honeycrisp', 'Pink Lady', 'Red Delicious'),
-    price     = seq(1, 4, 0.5), # $ per pound 
+    price     = seq(1, 4, 0.5), # $ per pound
     freshness = c('Excellent', 'Average', 'Poor')
 )
 
@@ -21,19 +21,19 @@ design <- cbc_design(
     profiles = profiles,
     n_resp   = 2000, # Number of respondents
     n_alts   = 3,    # Number of alternatives per question
-    n_q      = 8     # Number of questions per respondent
+    n_q      = 6     # Number of questions per respondent
 )
 
 head(design) # preview
 
-# Add image names matched to the apple type 
+# Add image names matched to the apple type
 # (we'll use these to display images in the survey)
 image_names <- data.frame(
     type = c('Fuji', 'Gala', 'Honeycrisp', 'Pink Lady', 'Red Delicious'),
-    image = c('fuji.jpg', 'gala.jpg', 'honeycrisp.jpg', 'pinkLady.jpg', 
+    image = c('fuji.jpg', 'gala.jpg', 'honeycrisp.jpg', 'pinkLady.jpg',
           'redDelicious.jpg')
 )
-design <- design %>% 
+design <- design %>%
     left_join(image_names, by = "type")
 head(design) # preview
 
